2ac1e0684d902713dc55234f0f6740dc904daf8c
AkashRaikwar <73732062+AkashRaikwar@users.noreply.github.com>
2024-04-07 20:45:23 +0530

    Add files via upload

---
+++ services.js
@@ -0,0 +1,9 @@
+function back(){
+    window.location.href = "service.html";
+}
+function reroute(){
+    window.location.href = "bookings.html";
+}
+function goToHome(){
+    window.location.href = "Home.html";
+}
